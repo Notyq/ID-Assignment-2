5264ee6146400f5d80ab19d3283e06529b28a3ea
duicchead <s10223127@connect.np.edu.sg>
2022-02-18 00:09:35 +0800

    CONTACT PAGE DONE; FORM SUBMISSION API WORKING

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,50 @@
+const APIKEY = "620e578234fd621565858735";
+
+$(document).ready(function () {
+    $("#feedbackformsubmit").on("click",function(e) {
+        e.preventDefault();
+
+        let name = $("#fname").val();
+        console.log(name);
+        let email = $("#email").val();
+        console.log(email);
+        let subject = $("#subject").val();
+        console.log(subject);
+        let message = $("#message").val();
+        console.log(message);
+        
+        let jsondata ={
+            "name": name,
+            "email": email,
+            "subject": subject,
+            "message": message
+        }
+        
+        var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://idasg2database-a96a.restdb.io/rest/feedbackform",
+        "method": "POST",
+        "headers": {
+        "content-type": "application/json",
+        "x-apikey": APIKEY,
+        "cache-control": "no-cache"
+        },
+        "processData": false,
+        "data": JSON.stringify(jsondata)
+        }
+
+        $.ajax(settings).done(function (response) {
+        console.log(response);
+        });
+
+
+
+    });
+
+})
+
+
 function msgfn()
 {
     var name = document.getElementById("fname").value;
@@ -9,22 +56,4 @@ function msgfn()
 
 }
 
-const contactForm = document.getElementById('contactForm');
 
-contactForm.addEventListener('submit', function(e){
-    e.preventDefault();
-
-    const formData = new FormData(this);
-
-    fetch('form.php', {
-        method: 'post',
-        body: formData
-    }).then(function (response) {
-        return response.text();
-    }).then(function (text) {
-        console.log(text);
-    }).catch(function(error){
-        console.error(error);
-    })
-
-});
