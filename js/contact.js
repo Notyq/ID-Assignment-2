9c79054c2c52dd5bd9800e1597f454d642bfd1a0
duicchead <s10223127@connect.np.edu.sg>
2022-02-17 19:45:29 +0800

    adding api in the contact page form; faulty

--- js/contact.js
+++ js/contact.js
@@ -8,3 +8,23 @@ function msgfn()
     alert("Your Name: " + name + "\nYour E-Mail: " + email + "\nSubject: " + subject + "\nYour message: " + msg);
 
 }
+
+const contactForm = document.getElementById('contactForm');
+
+contactForm.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const formData = new FormData(this);
+
+    fetch('form.php', {
+        method: 'post',
+        body: formData
+    }).then(function (response) {
+        return response.text();
+    }).then(function (text) {
+        console.log(text);
+    }).catch(function(error){
+        console.error(error);
+    })
+
+});
