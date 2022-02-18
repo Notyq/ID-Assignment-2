b02f8d44322ecfb3f30887769ecabfb6c97fe585
duicchead <s10223127@connect.np.edu.sg>
2022-02-18 16:33:28 +0800

    login & signup fully done

--- js/loginpage.js
+++ js/loginpage.js
@@ -39,7 +39,8 @@ $(document).ready(function (){
                 console.log(`${response[i].email}`, `${response[i].password}`);
                 if(`${response[i].email}` === loginemail && `${response[i].password}` === loginpassword){
                     loginSuccess = true;
-                    alert("Login Success!")
+                    alert("Login Success!\nClick OK to be redirected to the home page")
+                    window.location.href = "Home.html";
                     break;
                 }
 
@@ -64,7 +65,8 @@ $(document).ready(function () {
     $("#signupid").on("click",function(e) {
         e.preventDefault();
 
-        
+        var uniqueemail =  null;
+        console.log(uniqueemail);
 
         let email = $("#email").val();
         let emailconfirm = $("#emailconfirm").val();
@@ -81,36 +83,80 @@ $(document).ready(function () {
         if(email === emailconfirm && password === passwordconfirm){
 
             if(email.includes("@")){
-                
-                let jsondata ={
-                    "email": email,
-                    "password": password,
-                }
-    
+
                 var settings = {
                 "async": true,
                 "crossDomain": true,
                 "url": "https://idasg2databaselogin-8a41.restdb.io/rest/signuplogin",
-                "method": "POST",
+                "method": "GET",
                 "headers": {
                 "content-type": "application/json",
                 "x-apikey": APIKEY,
                 "cache-control": "no-cache"
-                },
-                "processData": false,
-                "data": JSON.stringify(jsondata)
+                }
                 }
                 
-                $.ajax(settings).done(function (response) {
-                console.log(response);
+                $.ajax(settings).done(function (response){
+                    console.log(response);
+                    for(let i=0; i < response.length; i++){
+
+                        if(`${response[i].email}` === email){
+                            
+                            console.log('------------------');
+                            console.log(`${response[i].email}`);
+                            console.log(typeof(`${response[i].email}`));
+                            console.log(email);
+                            console.log(typeof(email));
+                            var uniqueemail = false;
+                            break;
+
+                        }else{
+                            var uniqueemail = true;
+                        }
+
+                        
+                    }
+
+                    console.log(uniqueemail);
+
+                    if(uniqueemail === true){
+
+                        let jsondata ={
+                            "email": email,
+                            "password": password,
+                        }
+                
+                        var settings = {
+                        "async": true,
+                        "crossDomain": true,
+                        "url": "https://idasg2databaselogin-8a41.restdb.io/rest/signuplogin",
+                        "method": "POST",
+                        "headers": {
+                        "content-type": "application/json",
+                        "x-apikey": APIKEY,
+                        "cache-control": "no-cache"
+                        },
+                        "processData": false,
+                        "data": JSON.stringify(jsondata)
+                        }
+                        
+                        $.ajax(settings).done(function (response) {
+                        console.log(response);
+                        });
+        
+                        //window.location.href = "LogInPage.html";
+                        alert("Account Created!")
+                    }
+    
+                    else if (uniqueemail === false){
+                        alert("Email is already in use!")
+                    }
+
+
                 });
 
-                //window.location.href = "LogInPage.html";
-                alert("Account Created!")
                 
-
                 
-
             }
 
             else{
